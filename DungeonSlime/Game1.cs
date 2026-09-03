de7098179bebe0d3ab7be4c82ac0de84b0a7643a
BMO <kaidynfleetwood@gmail.com>
2026-09-02 23:08:17 -0400

    PNG now bounces!

--- DungeonSlime/Game1.cs
+++ DungeonSlime/Game1.cs
@@ -13,24 +13,25 @@ public class Game1 : Game
 
     // Effects
     private SpriteBatch _spriteBatch;
-    private Texture2D _logo;
+
     private Song _bgm;
     private Color previousColor;
     private Color targetColor = Color.CornflowerBlue;
     private Color backgroundColor;
 
     // Frame Stuff
+    private double _elapsedTime = 0;
+    private int _frameRateTracker = 0;
     private int frameCounter = 0;
     private const int targetFrames = 30;
 
-    // Gravity and Movement
-    private Rectangle bound;
-    private Rectangle bound2;
-    private Vector2 velocity;
-    private const int gravity = 2000;
+    // Bouncing Ball Info
+    private Vector2 _ballPosition;
+    private Vector2 _ballVelocity;
+    private Texture2D _logo;
 
 
-
+    // make it like galaga and add sprites that look like: >:(
     public Game1()
     {
         _graphics = new GraphicsDeviceManager(this);
@@ -49,6 +50,9 @@ public class Game1 : Game
 
         Window.AllowUserResizing = true;
 
+        _graphics.PreferredBackBufferWidth = 1600;
+        _graphics.PreferredBackBufferHeight = 1000;
+
         base.Initialize();
     }
 
@@ -58,43 +62,89 @@ public class Game1 : Game
 
         _logo = Content.Load<Texture2D>(@"images\download");
 
-        _bgm = Content.Load<Song>("arpmedia-club-party-dance-music-569466");
+        _bgm = Content.Load<Song>("Soulja Boy Tell'em - Turn My Swag On (Official Video)");
 
         MediaPlayer.IsRepeating = true;
         MediaPlayer.Volume = 0.5f;
         MediaPlayer.Play(_bgm);
+
+        // Position the ball in the center of the screen
+        _ballPosition.X = GraphicsDevice.Viewport.Width / 2;
+        _ballPosition.Y = GraphicsDevice.Viewport.Height / 2;
+
+        // Give the ball a random velocity
+        System.Random rand = new();
+        _ballVelocity.X = (float)rand.NextDouble();
+        _ballVelocity.Y = (float)rand.NextDouble();
+        _ballVelocity.Normalize();
+        _ballVelocity *= 1000;
+
+        Console.WriteLine(_ballVelocity.X);
+        Console.WriteLine(_ballVelocity.Y);
+
         // TODO: use this.Content to load your game content here
     }
 
-    protected void playerMovement()
-    {
+    // protected void playerMovement()
+    // {
         
-        KeyboardState pressedKey = Keyboard.GetState();
+    //     KeyboardState pressedKey = Keyboard.GetState();
 
-        if (pressedKey.IsKeyDown(Keys.W) || pressedKey.IsKeyDown(Keys.Up))
-        {
-            Console.WriteLine("Player Jumps");
-        }
-        if (pressedKey.IsKeyDown(Keys.A) || pressedKey.IsKeyDown(Keys.Left))
-        {
-            Console.WriteLine("Player Moves Left");
-            velocity.X = -100;
-        }
-        if (pressedKey.IsKeyDown(Keys.D) || pressedKey.IsKeyDown(Keys.Right))
-        {
-            Console.WriteLine("Player Moves Right");
-            velocity.X = 100;
-        }
+    //     if (velocity.Y > 0+_logo.Height && (pressedKey.IsKeyDown(Keys.W) || pressedKey.IsKeyDown(Keys.Up)))
+    //     {
+    //         Console.WriteLine("Player Jumps");
+    //         velocity.Y -= 100;
+    //     }
+    //     if (velocity.Y < Window.ClientBounds.Height - _logo.Height && (pressedKey.IsKeyDown(Keys.S) || pressedKey.IsKeyDown(Keys.Down)))
+    //     {
+    //         Console.WriteLine("Player Falls");
+    //         velocity.Y += 100;
+    //     }
+    //     if (velocity.X > 0+_logo.Width && (pressedKey.IsKeyDown(Keys.A) || pressedKey.IsKeyDown(Keys.Left)))
+    //     {
+    //         Console.WriteLine("Player Moves Left");
+    //         velocity.X -= 100;
+    //     }
+    //     if (velocity.X < Window.ClientBounds.Width - _logo.Width && (pressedKey.IsKeyDown(Keys.D) || pressedKey.IsKeyDown(Keys.Right)))
+    //     {
+    //         Console.WriteLine("Player Moves Right");
+    //         velocity.X += 100;
+    //     }
     
 
-    }
+    // }
+
 
     protected override void Update(GameTime gameTime)
     {
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
 
-        playerMovement();
+        _ballPosition += _ballVelocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+        if(_ballPosition.X < GraphicsDevice.Viewport.X || _ballPosition.X > GraphicsDevice.Viewport.Width - _logo.Bounds.Width)
+        {
+            _ballVelocity.X *= -1;
+        }
+        if (_ballPosition.Y < GraphicsDevice.Viewport.Y || _ballPosition.Y > GraphicsDevice.Viewport.Height - _logo.Bounds.Height)
+        {
+            _ballVelocity.Y *= -1;
+        }
+
+        _elapsedTime += gameTime.ElapsedGameTime.TotalSeconds;
+        _frameRateTracker++;
+
+        if(_elapsedTime >= 1.0)
+        {
+            Window.Title = $"FPS: {_frameRateTracker}";
+            _frameRateTracker = 0;
+            _elapsedTime -= 1.0;
+        }
+
+        // Console.WriteLine($"Position: {_ballPosition} Velocity: {_ballVelocity}");
+        // Console.WriteLine($"Logo Width: {_logo.Bounds.Width}, Logo Height: {_logo.Bounds.Height}");
+
+        //playerMovement();
         base.Update(gameTime);
     }
 
@@ -122,9 +172,7 @@ public class Game1 : Game
         _spriteBatch.Begin();
 
         //Draw the texture
-        _spriteBatch.Draw(_logo, new Vector2(Window.ClientBounds.Width - _logo.Width,
-                                             Window.ClientBounds.Height - _logo.Height) 
-                                             * .5f, Color.White);
+        _spriteBatch.Draw(_logo, _ballPosition, Color.White);
 
         _spriteBatch.End();
         
